5e8feaefa571aae89ee953af16cac2382f61f628
Guilherme França Fernandes <guilfranca27@gmail.com>
2025-02-25 13:36:57 -0300

    Aula 04 Desenvolvendo o temporizador - finalizado

--- Aula04/Fokus-projeto-base/script.js
+++ Aula04/Fokus-projeto-base/script.js
@@ -5,10 +5,20 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+
 const musicaFocoInput = document.querySelector('#alternar-musica');
 // Criação de um objeto nativo do javascript Audio
 const musica = new Audio('./sons/luna-rise-part-one.mp3'); // readFile() Leitura de quando o arquivo for utilizado
 // o audio possui 6min e normalmente quando ela termina, a mesma para, então utilizaremos loop para que a música não pare de tocar
+const beep = new Audio('./sons/beep.mp3');
+const play = new Audio('./sons/play.mp3');
+const pause = new Audio('./sons/pause.mp3');
+
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
 musica.loop = true;
 
 // change é utilizado em inputs do tipo check-box
@@ -42,6 +52,38 @@ function alterarContexto(contexto) {
                 <strong class="app__title-strong">Faça uma pausa longa.</strong>`
     }
 }
+
+// foi criada uma função dentro de uma constante
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        zerar();
+        beep.play();
+        alert('Tempo finalizado!');
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos);
+}
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        zerar();
+        pause.play();
+        return
+    }
+    // é utilizado o método setInterval que utiliza dois parametros o método que desa ser executado e o tempo em milissegundos
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    play.play();
+}
+
+function zerar() {
+    // interrompe a execução de um código
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
+
+// quando a função está armazenada em uma variável não é necessário utilizar () depois da função e a função só pode ser chamada depois da criação da mesma
+startPauseBt.addEventListener("click", iniciarOuPausar);
 
 focoBt.addEventListener("click", function() {
     alterarContexto('foco');
